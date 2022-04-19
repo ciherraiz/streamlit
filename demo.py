47748052b09639fa8f31dfbbd81ce2ac38115cfc
ciherraiz <ciherraiz@gmail.com>
2022-04-19 09:39:22 +0200

    slider

--- demo.py
+++ demo.py
@@ -1,18 +1,27 @@
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-import plotly.express as px
 import streamlit as st
-from datetime import datetime
+from datetime import time, datetime
 
-st.header('st.write')
+# https://share.streamlit.io/streamlit/30days
 
-st.write('Hello, *World!* :sunglasses:')
+st.header('st.slider')
+st.subheader('Slider')
 
-st.write(1234)
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
 
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
+st.subheader('Range slider')
+
+values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+st.subheader('Range time slider')
+
+appointment = st.slider('Schedule your appointment:', value=(time(11,30), time(12,45)))
+st.write("You're scheduled for: ", appointment)
+
+st.subheader('Datetime slider')
+
+start_time = st.slider('When do you start?', value=datetime(2020, 1, 1, 9, 30), format="MM/DD/YY - hh:mm")
+st.write('Start time:', start_time)
