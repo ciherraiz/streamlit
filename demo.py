22babd17e407141d386701e244e49696efd2c096
ciherraiz <ciherraiz@gmail.com>
2022-04-19 09:55:39 +0200

    line_chart

--- demo.py
+++ demo.py
@@ -5,23 +5,8 @@ from datetime import time, datetime
 
 # https://share.streamlit.io/streamlit/30days
 
-st.header('st.slider')
-st.subheader('Slider')
+st.header('Line chart')
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 
-st.subheader('Range slider')
-
-values = st.slider('Select a range of values', 0.0, 100.0, (25.0, 75.0))
-st.write('Values:', values)
-
-st.subheader('Range time slider')
-
-appointment = st.slider('Schedule your appointment:', value=(time(11,30), time(12,45)))
-st.write("You're scheduled for: ", appointment)
-
-st.subheader('Datetime slider')
-
-start_time = st.slider('When do you start?', value=datetime(2020, 1, 1, 9, 30), format="MM/DD/YY - hh:mm")
-st.write('Start time:', start_time)
+st.line_chart(chart_data)
